a40ef0773f11727d2f5ce9770cebed1964b42abb
SannyPaul <sannypaulgeraldino11@gmail.com>
2024-05-07 18:07:46 +0800

    landpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
-import { AfterLoginComponent } from '../after-login/after-login.component';
-import { ErrorPageComponent } from '../error-page/error-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landingPage', pathMatch: 'full'},
   { 
-    path: 'landingPage',
+    path: 'landingPage', 
     component: LandingPageComponent,
-  },
-]
-
+  }
+];
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
