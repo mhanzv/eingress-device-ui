d166789fc380fa36550ce6b48ee59672386a650f
SannyPaul <sannypaulgeraldino11@gmail.com>
2024-05-07 14:00:21 +0800

    first commit

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { AfterLoginComponent } from '../after-login/after-login.component';
+import { ErrorPageComponent } from '../error-page/error-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'landingPage', pathMatch: 'full'},
+  { 
+    path: 'landingPage',
+    component: LandingPageComponent,
+  },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
